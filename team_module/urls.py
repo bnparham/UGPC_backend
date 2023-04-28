b32861eb10f1d21a579777ef646800a380aa326c
bnparham <parhambn.workmail@gmail.com>
2023-04-28 20:10:58 +0430

    normal user can leave team feature add to project

--- team_module/urls.py
+++ team_module/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import inviteEmailView,acceptInvite,removeUserFromTeamView,rejectInvite
+from .views import inviteEmailView,acceptInvite,removeUserFromTeamView,rejectInvite,leaveTeam
 
 urlpatterns = [
     path("send-invite/", inviteEmailView.as_view(), name="inviteView"),
     path('accept-invite/', acceptInvite.as_view(), name="accept_invite"),
     path('remove-user/', removeUserFromTeamView.as_view(), name="remove_user"),
     path('reject-user/', rejectInvite.as_view(), name="reject-invite"),
+    path('leave-team/', leaveTeam.as_view(), name="leave-team"),
 ]
--- team_module/views.py
+++ team_module/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.shortcuts import render,redirect,reverse
 from django.views import View
 from account_module.models import User
@@ -18,7 +19,7 @@ class inviteEmailView(View):
                 # invite is send
                 request.session["send_invite"] = True
                 reciver: User = User.objects.get(email__iexact=email)
-                newInvite, created = inviteModel.objects.get_or_create(sender=self.request.user, reciver=reciver, is_accept=False)
+                newInvite, created = inviteModel.objects.get_or_create(sender=self.request.user, reciver=reciver, is_accept=False, is_reject=False)
                 if(created):
                     newInvite.invite_id = get_random_string(25)
                     newInvite.save()
@@ -40,15 +41,15 @@ class acceptInvite(View):
                 reciver.save()
                 findTeam.teamMate1 = reciver
                 findTeam.save()
-                findInvite = inviteModel.objects.get(reciver=reciver,sender=sender,is_accept=False)
-                findInvite.is_accept=True
+                findInvite = inviteModel.objects.filter(reciver=reciver, sender=sender, is_accept=False, is_reject=False).last()
+                findInvite.is_accept = True
                 findInvite.save()
         elif(findTeam.teamMate2 == None):
             findTeam.teamMate2 = reciver
             reciver.has_team = True
             reciver.save()
             findTeam.save()
-            findInvite = inviteModel.objects.get(reciver=reciver, sender=sender,is_accept=False)
+            findInvite = inviteModel.objects.filter(reciver=reciver, sender=sender,is_accept=False, is_reject=False).last()
             findInvite.is_accept = True
             findInvite.save()
         else:
@@ -78,8 +79,24 @@ class rejectInvite(View):
         reciver = request.POST.get("reciver")
         sender = User.objects.get(email__iexact=sender)
         reciver = User.objects.get(email__iexact=reciver)
-        findInvite = inviteModel.objects.get(sender=sender,reciver=reciver)
+        findInvite = inviteModel.objects.filter(sender=sender,reciver=reciver).last()
         findInvite.is_reject = True
         findInvite.save()
+        return redirect(reverse("userPanel"))
+
+class leaveTeam(View):
+    def post(self, request):
+        get_user = request.POST.get("user")
+        user = User.objects.get(email__iexact=get_user)
+        findTeam = teamsModel.objects.get(Q(teamMate1=user) | Q(teamMate2=user))
+        if(findTeam is not None):
+            if(findTeam.teamMate1 == user):
+                findTeam.teamMate1 = None
+                user.has_team = False
+            elif(findTeam.teamMate2 == user):
+                findTeam.teamMate2 = None
+                user.has_team = False
+            findTeam.save()
+            user.save()
         return redirect(reverse("userPanel"))
 
